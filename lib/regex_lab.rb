db11c1fbac70a425cf146addbb874fe3ac4e6d15
Dominic Chapman <dominicchapman@me.com>
2017-10-07 19:58:29 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,6 +11,7 @@ end
 
 def words_five_letters_long(text)
   # returns an array of words that are five letters long
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
