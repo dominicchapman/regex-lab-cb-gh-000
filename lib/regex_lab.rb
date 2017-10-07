aaa1fbe919e4d4c73ea392128ea5074a6b876ef5
Dominic Chapman <dominicchapman@me.com>
2017-10-07 19:54:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,6 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  # returns an array with the words starting with 'un' and ending with 'ing'
   text.scan
 end
 
