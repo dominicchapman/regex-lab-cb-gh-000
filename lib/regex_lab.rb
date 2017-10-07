6aa4591615a9a34e0b934f7b97160a315384f03f
Dominic Chapman <dominicchapman@me.com>
2017-10-07 19:56:26 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,7 +10,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+  # returns an array of words that are five letters long
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
