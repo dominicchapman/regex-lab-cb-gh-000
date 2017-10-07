6c30871f86ff958f3dfde61b0b521042291c9596
Dominic Chapman <dominicchapman@me.com>
2017-10-07 19:54:15 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
-
-  # !!word.match(/^[aeiouAEIOU]\w+/)
+  # returns true for words starting with a vowel
+  !!word.match(/^[aeiouAEIOU]\w+/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
